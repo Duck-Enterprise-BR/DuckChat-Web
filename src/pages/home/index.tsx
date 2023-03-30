fa08b6b354043fb411fa9aaa72557c892e8b1e71
SrWalkerB <srwalkerbdevs@gmail.com>
2023-03-30 01:32:21 -0300

    iniciando tela de chat

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,79 @@
+import "./style.css"
+import databaseMock from "./mock.json";
+import { useEffect, useState } from "react";
+
 export function Home(){
+    const [contact, setContact] = useState(databaseMock.contacts);
+    const [chatActive, setChatActive] = useState();
+
+    useEffect(() => {
+    }, [])
+
+    function ContactItem({ name, lastMessage, messageCount, date, img}: any){
+        return (
+            <>
+                <div className="container-contact-item" onClick={() => { setChatActive(name) }}>
+                    <div className="container-contact-item-profile">
+                        <img
+                            src={img} 
+                            width={90}
+                            height={80}
+                        />
+                    </div>
+
+                    <div className="container-contact-item-message">
+                        <div className="container-contact-item-message-title">
+                            <h3>{name}</h3>
+                            <h4>{date}</h4>
+                        </div>
+
+                        <div className="container-contact-item-message-body">
+                            <h5>{lastMessage}</h5>
+                            
+                            <div className="container-contact-item-message-body-count-message">
+                                <h6>{messageCount}</h6>
+                            </div>
+                        </div>
+                    </div>
+                 </div>
+            </>
+        )
+    }
+
+    function MessageSelectOneChat(){
+        return (
+            <h1>Selecione um chat!</h1>
+        )
+    }
+
     return (
-        <div>
-            <h1>Hello WOrld</h1>
+        <div className="container-page">
+            <div className="container-list-contact-option">
+                <div className="container-my-profile">
+                    <h2>SrWalkerB</h2>
+                </div>
+
+                <div className="container-contact">
+                    {
+                        contact.map((item, index) => {
+                            return (
+                                <ContactItem
+                                    key={index}
+                                    name={item.name} 
+                                    img={item.img}
+                                    lastMessage={item.lastMessage} 
+                                    messageCount={item.messageCount} 
+                                    date={item.date} 
+                                />
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
+            <div className="container-chats">
+                { chatActive ? <h1>{chatActive }</h1> : <MessageSelectOneChat />}
+            </div>
         </div>
     )
 }
--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export type MethodRequest = "GET" | "POST" | "PUT" | "DELETE";
 
 export function HttpService(router: string, method: MethodRequest, data?: any){
-    const baseUrl = `http://localhost:3000/${router}`
+    const baseUrl = `http://localhost:4000/${router}`
 
     return axios({
         url: baseUrl,
